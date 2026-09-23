1abf221e9a26c9e91be3bd4a465a2de4d5e88ac4
manuelalramos <manuela.lima.r@outlook.com>
2026-09-23 11:43:32 -0300

    feat : add tabela e cards de produtos utilitizando fetch api para edicao

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,45 +1,116 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+
 import type { TipoProduto } from "../../types/types";
-import { data } from "react-router";
+
+import { Link } from 'react-router';
+import CardProduto from "../../components/CardProduto";
+
 
 export default function Produtos() {
-  
-  //Para alterar o titulo da página: 
 
-  document.title = "Produtos" 
+  // Para alterar o título da página:
+  document.title = "Produtos";
 
-  const [Produtos, setProdutos] = useState<TipoProduto[]>([]);
+  const [produtos, setProdutos] = useState<TipoProduto[]>([]);
 
-  useEffect ( ()=> {
+  useEffect(() => {
 
-    const carregaProdutos = async ()=>{
-      try{
-          const response = await fetch("http://localhost:3001/produtos");
-        
-          if (!response.ok){
-          throw new Error("Erro na listagem dos produtos!")
+    const carregaProdutos = async () => {
+
+      try {
+
+        const response = await fetch("http://localhost:3001/produtos");
+
+        if (!response.ok) {
+          throw new Error("Erro na listagem dos produtos!");
         }
 
-        const data:TipoProduto [] =  await response.json();
-        
-        console.log(data)
+        const data: TipoProduto[] = await response.json();
 
-  
-      } catch(error) {
+        console.log(data);
+
+        setProdutos(data);
+
+      } catch (error) {
+
         console.error(error);
-      }
-      
 
-    }
-    
+      }
+
+    };
+
     carregaProdutos();
 
   }, []);
 
-
   return (
     <main>
-        <h2>Produtos</h2>
+
+      <h2>Produtos</h2>
+
+      <div style={{display: "flex", gap: "15px", flexWrap: "wrap"}}>
+      {produtos.map((produto)=>(
+        <CardProduto key={produto.id} produto={produto}/>
+        
+      ))}
+      </div>
+
+     
+
+      <div>
+
+        <table
+          border={1}
+          style={{
+            width: "100%",
+            borderCollapse: "collapse"
+          }}
+        >
+
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NOME</th>
+              <th>PREÇO</th>
+              <th>DESCRIÇÃO</th>
+              <th>AVATAR</th>
+              <th>AÇÕES</th>
+            </tr>
+          </thead>
+
+          <tbody>
+
+            {produtos.map((produto) => (
+              <tr key={produto.id}>
+
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>{produto.preco}</td>
+                <td>{produto.descricao}</td>
+
+                <td><img src={produto.avatar} alt={produto.nome} width={80}/>
+                </td>
+                
+                <td><Link to={`/editar-produtos/${produto.id}`}>EDITAR</Link>/EXCLUIR</td>
+
+
+              </tr>
+            ))}
+
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td colSpan={6}>
+                Quantidade de registros: {produtos.length}
+              </td>
+            </tr>
+          </tfoot>
+
+        </table>
+
+      </div>
+
     </main>
-  )
+  );
 }
