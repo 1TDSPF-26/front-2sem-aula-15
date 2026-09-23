2447e58d288a412e1e262bff069294c4f18aff7d
anapmagalhaess <bruclaramagalhaes2015@gmail.com>
2026-09-23 11:17:09 -0300

    feat: Criando listagem de dados atraves da tabela.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import type { TipoProduto } from "../../types/types";
+import { Link } from "react-router";
 
 export default function Produtos() {
     // Para alterar o título da página:
@@ -19,7 +20,7 @@ export default function Produtos() {
                 }
 
                 const data: TipoProduto[] = await response.json();
-                console.log(data);
+                setProdutos(data);
 
             } catch (error) {
                 console.error(error);
@@ -31,14 +32,42 @@ export default function Produtos() {
 
     }, []);
 
-
     //Apresente a lista de produtos em CARDS utilizando PROPS...
 
     return (
         <main>
             <h2>Produtos</h2>
             <div>
-                <table style={{ width: "100%", borderCollapse: "collapse" ,border: "1px solid black" }}></table>
+                <table border={1} style={{ width: "100%", borderCollapse: "collapse" ,border: "1px solid black" }}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOME</th>
+                            <th>PREÇO</th>
+                            <th>DESCRIÇÃO</th>
+                            <th>AVATAR</th>
+                            <th>AÇÕES</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {produtos.map( (produto)=>(
+                            <tr key={produto.id}>
+                                <td>{produto.id}</td>
+                                <td>{produto.nome}</td>
+                                <td>{produto.preco}</td>
+                                <td>{produto.descricao}</td>
+                                <td><img src={produto.avatar} alt={produto.nome} width={40} /></td>
+                                <td> <Link to={`/editar-produtos-${produto.id}/`}>EDITAR</Link>/ EXCLUIR</td>
+                            </tr>
+
+                        ))}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan={6}>Quantidade de registros : {produtos.length}</td>
+                        </tr>
+                    </tfoot>
+                </table>
             </div>
         </main>
     )
