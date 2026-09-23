6e9ef951f6c7f46e86a7b086be2f9cfa255896c4
enzoestanguera <enzo.estanguera@gmail.com>
2026-09-23 11:53:48 -0300

    feat: Aprendendo react icons e correção do projeto

--- my-app/src/components/CardProduto/index.tsx
+++ my-app/src/components/CardProduto/index.tsx
@@ -1,21 +1,24 @@
-import type { TipoProduto } from "../../types/types";
 import { Link } from "react-router";
+import type { TipoProduto } from "../../types/types";
+import { FaEdit as Editar} from "react-icons/fa";
+import { MdDeleteForever as Excluir} from "react-icons/md";
 
-type Props ={
-    produto : TipoProduto
+type Props = {
+    produto: TipoProduto
 }
 
-export default function CardProduto({produto}: Props) {
+
+export default function CardProduto({produto}:Props ) {
   return (
-    <div>
+    <div style={{ border: "1px solid #ccc", padding: "16px", borderRadius: "8px", width: "200px" }}>
         <h3>{produto.nome}</h3>
-        <p>${produto.preco}</p>
+        <p>R${produto.preco}</p>
         <figure>
-            <img src={produto.avatar} alt={produto.nome} width={60}/>
+            <img src={produto.avatar} alt={produto.nome} width={80}/>
             <figcaption>{produto.descricao}</figcaption>
         </figure>
         <div>
-            <span><Link to={`/editar-produtos/${produto.id}`}>Editar</Link></span> - <span>Excluir</span>
+            <span><Link to={`/editar-produtos/${produto.id}`}><Editar/></Link></span> - <span><Excluir/></span>
         </div>
     </div>
   )
--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,79 +1,92 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import type { TipoProduto } from "../../types/types";
 import { Link } from "react-router";
 import CardProduto from "../../components/CardProduto";
+import { FaEdit as Editar} from "react-icons/fa";
+import { MdDeleteForever as Excluir} from "react-icons/md";
 
 export default function Produtos() {
-    // Para alterar o título da página:
-    document.title = "Produtos"
+  // Para alterar o título da página:
+  document.title = "Produtos";
 
-    const [produtos, setProdutos] = useState<TipoProduto[]>([]);
+  const [produtos, setProdutos] = useState<TipoProduto[]>([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    const carregaProdutos = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/produtos");
 
-        const carregaProdutos = async () => {
-
-            try {
-                const response = await fetch("http://localhost:3001/produtos");
-
-                if (!response.ok) {
-                    throw new Error(`Erro na listagem dos produtos: ${response.status} - ${response.statusText}`);
-                }
-
-                const data: TipoProduto[] = await response.json();
-                setProdutos(data);
-
-            } catch (error) {
-                console.error(error);
-            }
-
+        if (!response.ok) {
+          throw new Error(
+            `Erro na listagem dos produtos: ${response.status} - ${response.statusText}`,
+          );
         }
 
-        carregaProdutos();
+        const data: TipoProduto[] = await response.json();
+        setProdutos(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    }, []);
+    carregaProdutos();
+  }, []);
 
-    //Apresente a lista de produtos em CARDS utilizando PROPS...
+  //Apresente a lista de produtos em CARDS utilizando PROPS...
 
-    return (
-        <main>
-            <h2>Produtos</h2>
-            {produtos.map((produto)=>
-            <CardProduto key={produto.id} produto={produto}/>
-            )}
-            
+  return (
+    <main>
+      <h2>Produtos</h2>
 
-            <div>
-                <table border={1} style={{ width: "100%", borderCollapse: "collapse" ,border: "1px solid black" }}>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>NOME</th>
-                            <th>PREÇO</th>
-                            <th>DESCRIÇÃO</th>
-                            <th>AVATAR</th>
-                            <th>AÇÕES</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {produtos.map( (produto)=>(
-                            <tr key={produto.id}>
-                                <td>{produto.id}</td>
-                                <td>{produto.nome}</td>
-                                <td>{produto.preco}</td>
-                                <td>{produto.descricao}</td>
-                                <td><img src={produto.avatar} alt={produto.nome} width={40}/></td>
-                                <td><Link to={`/editar-produtos/${produto.id}`}>EDITAR</Link>   / EXCLUIR</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td colSpan={6}>Quantidade de registros : {produtos.length}</td>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
-        </main>
-    )
+      <div style={{ display: "flex", gap: "16px", flexWrap: "wrap" }}>
+        {produtos.map((produto) => (
+          <CardProduto key={produto.id} produto={produto} />
+        ))}
+      </div>
+
+      <div>
+        <table
+          border={1}
+          style={{
+            width: "100%",
+            borderCollapse: "collapse",
+            border: "1px solid black",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NOME</th>
+              <th>PREÇO</th>
+              <th>DESCRIÇÃO</th>
+              <th>AVATAR</th>
+              <th>AÇÕES</th>
+            </tr>
+          </thead>
+          <tbody>
+            {produtos.map((produto) => (
+              <tr key={produto.id}>
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>{produto.preco}</td>
+                <td>{produto.descricao}</td>
+                <td>
+                  <img src={produto.avatar} alt={produto.nome} width={40} />
+                </td>
+                <td>
+                  <Link to={`/editar-produtos/${produto.id}`}><Editar/></Link> /
+                  <Excluir/>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={6}>Quantidade de registros : {produtos.length}</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </main>
+  );
 }
