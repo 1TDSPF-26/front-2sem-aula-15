b880337bb006153adfdaae9a2d17f99f456636ea
BryanC0staDev <bryancosta713@gmail.com>
2026-09-23 11:47:42 -0300

    feat : Cards de produtos sendo utilizados com fetch api para edição.

--- my-app/src/Types/types.ts
+++ my-app/src/Types/types.ts
@@ -1,8 +1,8 @@
-export type tipoProduto = [
-    id: string,
-    nome: string,
-    preco: number,
-    descricao: string,
-    avatar: string
+export type tipoProduto = {
+    id: string;
+    nome: string;
+    preco: number;
+    descricao: string;
+    avatar: string;
 
-]
+}
--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import type { tipoProduto } from "../../Types/types";
 import { Link } from "react-router";
+import CardProduto from "../../components/CardProduto";
 
 export default function Produtos() {
     // Para alterar o título da página:
@@ -31,39 +32,60 @@ export default function Produtos() {
 
 
     return (
-        <main>
-            <h2>Produtos</h2>
-            <div>
-                <table border={1} style={{width:"100%",borderCollapse:"collapse",border: "1px solid black"}}>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>NOME</th>
-                            <th>PRECO</th>
-                            <th>DESCRIÃO</th>
-                            <th>AVATAR</th>
-                            <th>AÇÕES</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {produtos.map((produto)=>(
-                            <tr key={produto.id}>
-                                <td>{produto.id}</td>
-                                <td>{produto.nome}</td>
-                                <td>{produto.preco}</td>
-                                <td>{produto.descricao}</td>
-                                <td><img src={produto.avatar} alt={produto.nome} width={40} /></td>
-                                <td><Link to={`/editar-produtos/${produto.id}`}>EDITAR</Link></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td colSpan={6}>Quantidade de Registros: {produtos.length}</td>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
-        </main>
-    )
+
+
+       <main>
+      <h2>Produtos</h2>
+
+      <div style={{ display: "flex", gap: "16px", flexWrap: "wrap" }}>
+        {produtos.map((produto) => (
+          <CardProduto key={produto.id} produto={produto} />
+        ))}
+      </div>
+
+      <div>
+        <table
+          border={1}
+          style={{
+            width: "100%",
+            borderCollapse: "collapse",
+            border: "1px solid black",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NOME</th>
+              <th>PREÇO</th>
+              <th>DESCRIÇÃO</th>
+              <th>AVATAR</th>
+              <th>AÇÕES</th>
+            </tr>
+          </thead>
+          <tbody>
+            {produtos.map((produto) => (
+              <tr key={produto.id}>
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>{produto.preco}</td>
+                <td>{produto.descricao}</td>
+                <td>
+                  <img src={produto.avatar} alt={produto.nome} width={40} />
+                </td>
+                <td>
+                  <Link to={`/editar-produtos/${produto.id}`}>EDITAR</Link> /
+                  EXCLUIR
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={6}>Quantidade de registros : {produtos.length}</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </main>
+  );
 }
